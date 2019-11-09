6f427d11f169d30fde833e746ed55139205af912
PTLam <phamlamptl@gmail.com>
2019-11-09 22:54:58 +0300

    finish homework1

--- homework1/3 ЧАСТЬ/32.js
+++ homework1/3 ЧАСТЬ/32.js
@@ -6,5 +6,10 @@ let obj = {
     model: '',
     resolution: '',
     color: '',
-}
+} // создали объект без значений
 
+// записываем значения черех prompt
+obj['brand'] = prompt('Your phone brand: ')
+obj['model'] = prompt('Your phone model: ')
+obj['resolution'] = prompt('Your phone resolution: ')
+obj['color'] = prompt('Your phone color: ')
